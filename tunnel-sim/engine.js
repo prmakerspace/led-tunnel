468a1dc7306782edc8dd98059269486f76d98f0f
Pat Long <plong00@gmail.com>
2017-06-17 20:02:29 -0700

    Configure UI and Socket and connect to relay server

--- tunnel-sim/engine.js
+++ tunnel-sim/engine.js
@@ -6,35 +6,88 @@ var SIM = (function(){
       console.log('LED SIMULATOR ACTIVATING...');
       config = config || {};
       if (Detector.webgl) {
-        init();
+        init(config);
         console.log('LED SIMULATOR ACTIVATED!');
       } else {
         console.error('WebGL detection Failed!');
         console.log('LED SIMULATOR ACTIVATION FAILED!');
         var warning = Detector.getWebGLErrorMessage();
-        document.getElementById('container').appendChild(warning);
+        $(config.WebGL.selector).append(warning);
       }
     }
   };
 
   // Private members
-  function init() {
-    socket.init();
-    world.init();
-    LEDs.init();
+  function init(config) {
+    config = config || {};
+    UI.init(config.UI);
+    Socket.init(config.Socket);
+    World.init(config.World);
+    LEDs.init(config.LEDs);
   };
 
-  var socket = {
-    init: function() {
+  var Socket = {
+    init: function(config) {
+      config = config || {};
+      this.isConnected = false;
       console.log('Connecting to fcserver...');
-      // @TODO: connect to the fcserver
-      // @TODO: when OPC message is received, call LEDs.update(...);
+      // Connect to a Fadecandy server running on the same computer, on a port configured to relay
+      this.connection = new WebSocket('ws://'+config.host+':'+config.port);
+
+      // Options needed for relaying
+      // setting binaryType is required to properly receive the OPC messages
+      this.connection.binaryType = 'arraybuffer';
+
+      // Socket event handlers
+      this.connection.onopen = this.connected;
+      this.connection.onclose = this.disconnected;
+      this.connection.onmessage = this.packetReceived;
+    },
+    connected: function(event) {
+      this.isConnected = true;
+      UI.setConnectionStatus('Connected');
       console.log('Connected to fcserver!');
+    },
+    disconnected: function(event) {
+      this.isConnected = false;
+      UI.setConnectionStatus('Disconnected');
+      console.log('Disconnected from fcserver!');
+    },
+    packetReceived: function(event) {
+      // @TODO: parse event and call LEDs.update(...);
+      console.log('Packet received =>', event);
     }
   };
 
-  var world = {
-    init: function() {
+  var UI = {
+    init: function(config) {
+      config = config || {};
+      this.HUD = $(config.selector || '#HUD');
+    },
+    setConnectionStatus(status) {
+      if (!this.connectionStatus && this.HUD) {
+        this.connectionStatus = $('<div class="connection-status"></div>');
+        this.HUD.append(this.connectionStatus);
+      }
+      if (this.connectionStatus) {
+        var oldStatus = this.connectionStatus.data('status');
+        if (status != oldStatus) {
+          this.connectionStatus.html(status);
+          if (oldStatus) {
+            this.connectionStatus.removeClass('status-'+oldStatus.toLowerCase());
+          }
+          if(status) {
+            this.connectionStatus.addClass('status-'+status.toLowerCase());
+          }
+          this.connectionStatus.data('status', status || null);
+        }
+      }
+    }
+  };
+
+  var World = {
+    init: function(config) {
+      config = config || {};
       console.log('Initializing simulation world...');
       // @TODO: initialize the simulation environment
       console.log('Simulation world initialized!');
@@ -46,8 +99,9 @@ var SIM = (function(){
   };
 
   var LEDs = {
-    init: function() {
-      console.log('Initializing Virutal LEDs...');
+    init: function(config) {
+      config = config || {};
+      console.log('Initializing Virtual LEDs...');
       // @TODO: initialize LED meshes, each with an individual texture/material
       console.log('Virtual LEDs initialized!');
     },
