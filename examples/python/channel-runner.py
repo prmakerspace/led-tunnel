595f6ec195ed2e28ff1f7864e21c26e1efc165d1
Pat Long <plong00@gmail.com>
2017-06-19 00:48:20 -0700

    Modified channel-runner python example

--- examples/python/channel-runner.py
+++ examples/python/channel-runner.py
@@ -6,8 +6,8 @@ client = opc.Client('localhost:7890')
 
 numChannels = 48
 ledsPerChannel = 64
-launchDelay = 0.25
-trailLength = 48
+launchDelay = 1.125
+trailLength = 32
 
 currentChannel = 0
 launched = {}
@@ -27,8 +27,8 @@ def do_launch(channel):
     return False
 
 def pixel_index(channel, index):
+    global ledsPerChannel
     return (channel * ledsPerChannel + index)
-        
 
 timer = time.time()
 while True:
@@ -43,12 +43,13 @@ while True:
         if (i in launched):
             start = launched[i] - 1
             for j in range(start, max(0, start-trailLength), -1):
-                index = pixel_index(i, j)
-                strength = (trailLength - start - j) / trailLength
-                pixels[index] = (127 - strength * 128, strength * 255, 191 + strength * 64)
+                if (j < ledsPerChannel):
+                    index = pixel_index(i, j)
+                    strength = (trailLength - start - j) / trailLength
+                    pixels[index] = (127 - strength * 128, strength * 255, 191 + strength * 64)
 
             launched[i] += 1
-            if (launched[i] > ledsPerChannel):
+            if (launched[i] > ledsPerChannel + trailLength):
                 launched[i] = 1
 
     client.put_pixels(pixels)
